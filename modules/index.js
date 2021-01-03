11eb8fd0527e19d138983d1efe1b7e5f3f4d3334
Orhan <orhancelik@windowslive.com>
2021-01-03 19:59:42 +0300

    module.exports "call helpers on this index.js with required"

---
+++ modules/index.js
@@ -0,0 +1,15 @@
+let math = require("./mathLib")
+
+let sayi1 = 10, sayi2 = 5;
+
+console.log(`${sayi1} + ${sayi2} = ${math.topla(10,5)}`)
+console.log(`${sayi1} - ${sayi2} = ${math.cikar(10,5)}`)
+console.log(`${sayi1} * ${sayi2} = ${math.carp(10,5)}`)
+console.log(`${sayi1} / ${sayi2} = ${math.bol(10,5)}`)
+
+/*
+10 + 5 = 15
+10 - 5 = 5
+10 * 5 = 50
+10 / 5 = 2
+*/
---
+++ modules/mathLib.js
@@ -0,0 +1,14 @@
+module.exports = {
+    topla: (sayi1,sayi2) => {
+        return sayi1 + sayi2
+    },
+    cikar: (sayi1,sayi2) => {
+        return sayi1 - sayi2
+    },
+    carp: (sayi1,sayi2) => {
+        return sayi1 * sayi2
+    },
+    bol: (sayi1,sayi2) => {
+        return sayi1 / sayi2
+    }
+}
